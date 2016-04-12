62de41b7b8eb6e5aec9fbbb19d91550266aa0792
Zachary Mays <zackmays@gmail.com>
2016-04-12 10:58:26 -0700

    Adds dropdown for categories; updates layout slightly

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,30 @@
 <div class="container text-center">
     <form class="form-inline">
         <div class="row">
-            <div class="form-group">
-                <label for="category">Category</label>
-                <input type="text" class="form-control" id="category" placeholder="Category">&nbsp;
+            <div class="col-sm-6 text-right">
+                <div class="form-group">
+                    <label for="category">Category</label>
+                    <select id="category" class="form-control">
+                        <option value="">Select a Category</option>
+                        <option value="newamerican">American (New)</option>
+                        <option value="tradamerican">American (Traditional)</option>
+                        <option value="asianfusion">Asian Fusion</option>
+                        <option value="brazilian">Brazilian</option>
+                        <option value="burmese">Burmese</option>
+                        <option value="chinese">Chinese</option>
+                        <option value="italian">Italian</option>
+                        <option value="japanese">Japanese</option>
+                        <option value="korean">Korean</option>
+                        <option value="mexican">Mexican</option>
+                        <option value="pizza">Pizza</option>
+                    </select>
+                </div>
             </div>
-            <div class="form-group">
-                <label for="zip">Zip Code</label>
-                <input type="text" class="form-control" id="zip" placeholder="Zip Code">
+            <div class="col-sm-6 text-left">
+                <div class="form-group">
+                    <label for="zip">Zip Code</label>
+                    <input type="text" class="form-control" id="zip" placeholder="Zip Code">
+                </div>
             </div>
         </div>
         <div class="row">
